3aa80708ac98ccf3e71b61d321a7abe6f9d3fb11
anushree <aushreebondia@gmail.com>
2023-09-02 19:10:43 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import Counter from './Counter';
+import ImageBox from './ImageBox';
 import './App.css';
 
 
 function App() {
   return (
+    <>
     <div className="blank">
       <Counter />
     </div>
+
+    <div className="imageBox">
+      <ImageBox />
+    </div>
+    </>
   );
 }
 
